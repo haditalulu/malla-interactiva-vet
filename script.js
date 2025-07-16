897266d6593ad10fb95527a6498569727eea9f44
haditalulu <lunamorenorff@gmail.com>
2025-07-16 11:06:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const ramos = document.querySelectorAll(".ramo");
-
-  ramos.forEach((ramo) => {
-    ramo.addEventListener("click", () => {
-      ramo.classList.toggle("aprobado");
-    });
-  });
-});
 
